399a06a5f4819b0c4f507ff02c932c1bc48821d1
vinicius souza <souzavinicius1993@gmail.com>
2015-01-06 17:17:14 -0400

    Modificacoes Marcos - Pontuacao

--- app/src/main/java/com/br/ksg/bancoDeDados/BancoDeDados.java
+++ app/src/main/java/com/br/ksg/bancoDeDados/BancoDeDados.java
@@ -10,7 +10,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class BancoDeDados extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 9;
+    public static final int DATABASE_VERSION = 10;
     public static final String DATABASE_NAME = "KSG";
 
     private static final String SQL_CREATE_TABLE_INGREDIENTES = "CREATE TABLE Ingredientes(" +
